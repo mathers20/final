6910e752966208d103636c19ddfe5fcd594f6247
Genaro Lorio <glorio@loopstudio.dev>
2023-02-02 14:09:27 -0300

    push all

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import{Text} from 'react-native';
+import {Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
 
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,43 +1,61 @@
-import { StackScreenProps } from "@react-navigation/stack";
-import React from "react";
+import { StackScreenProps } from '@react-navigation/stack';
+import React, { useState } from 'react';
 import { styles } from '../theme/appTheme';
-import{Button, ScrollView, Text,TextInput,View} from 'react-native';
+import {Alert, Button, ScrollView, Text,TextInput,View} from 'react-native';
 
-interface Props extends StackScreenProps<any, any> {};
+interface Props extends StackScreenProps<any, any> {}
 
 
 export const Login = ({navigation}:Props) => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const login = async () => {
+		try {
+			const fetchApiCall = await fetch('url');
+			const response = fetchApiCall.json();
+			console.log(response);
+			navigation.navigate('Profile');
+		} catch (error) {
+			Alert.alert('Error','Ha habido un error');
+		}
+	};
+
   return (
-    <ScrollView style={{flex:1, backgroundColor:"Grey"}}>
+    <ScrollView style={{flex:1, backgroundColor:'Grey'}}>
 		<View >
 			<View style={{paddingHorizontal: 30 }}>
 				<TextInput
 					style={styles.input}
 					placeholder="Email"
 					placeholderTextColor={'grey'}
-					autoCapitalize = 'none'
+					autoCapitalize = "none"
+					value={email}
+					onChangeText={e => setEmail(e)}
 				/>
 
 				<TextInput
 					style={styles.input}
 					placeholder="Contraseña"
 					placeholderTextColor={'grey'}
-					autoCapitalize = 'none'
+					autoCapitalize = "none"
+					value={password}
+					onChangeText={e => setPassword(e)}
 				/>
         </View>
         <View style ={styles.globalMargin}>
           <View>
             <Text>FALTA CHECKBOX</Text>
           </View>
-		
+
 			<Button
 				title="Login"
-				onPress={() => navigation.navigate('Profile')}
+				onPress={login}
 				/>
     	</View>
       </View>
 	</ScrollView>
-    
+
   );
 };
 
